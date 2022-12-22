0178253f7e66090ae546f8b356b1a9373b6bcafd
thongtran2910 <bathong2910@gmail.com>
2022-12-22 13:15:10 +0700

    update database connection

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const cors = require("cors");
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: "dpg-ceha0fp4reb94c0bt140-a",
-    user: "postgres",
-    password: "thong2910",
-    database: "smartbrain",
+    host: "dpg-ceha0fp4reb94c0bt140-a.singapore-postgres.render.com",
+    user: "face_recognition_user",
+    password: "0KuHvvBtIvBAAbsYkZiIJaN6JvEiJ6a0",
+    database: "face_recognition",
   },
 });
 const register = require("./controllers/register");
